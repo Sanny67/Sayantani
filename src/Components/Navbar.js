49ec706e11c155f4c0b7f955dd510d1b3be336d7
Sayantani <sayantanichatterjee67@gmail.com>
2019-11-08 10:35:15 +0530

    another link

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -8,6 +8,7 @@ class Navbar extends React.Component {
                 <a className="nav-link active" href="#">Home</a>
                 <a className="nav-link" href="#">Working from home</a>
                 <a className="nav-link" href="#">New Work</a>
+                <a className="nav-link" href="#">Another Link</a>
             </nav>
         );
     }
